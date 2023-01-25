49acaad93827d9ffd8b82c8fbc50785780b45682
Thiago2436 <thiagomartins.florestal@gmail.com>
2023-01-25 19:49:09 -0300

    Criptografia de senha

--- valida.php
+++ valida.php
@@ -1,12 +1,15 @@
 <?php
 session_start();
+include_once("conex.php");
 $btnLogin = filter_input(INPUT_POST, 'btnLogin', FILTER_SANITIZE_STRING);
     if ($btnLogin){
         $usuario = filter_input(INPUT_POST, 'usuario', FILTER_SANITIZE_STRING);
         $senha = filter_input(INPUT_POST, 'senha', FILTER_SANITIZE_STRING);
         echo " $usuario - $senha ";
         if((!empty($usuario)) AND (!empty($senha))){
-
+            //Gerar senha e criptografar
+            echo password_hash($senha, PASSWORD_DEFAULT);
+            //pesquisar cusuario no B.D
         } else {
             $_SESSION['msg'] = "LOGIN E SENHA : INCORRETO";
             header("Location: login.php");
