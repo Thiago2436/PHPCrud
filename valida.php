78766a5900a33968f2f581e70d532729f8a9afef
Thiago2436 <thiagomartins.florestal@gmail.com>
2023-01-26 15:11:17 -0300

    Update login

--- valida.php
+++ valida.php
@@ -8,8 +8,19 @@ $btnLogin = filter_input(INPUT_POST, 'btnLogin', FILTER_SANITIZE_STRING);
         echo " $usuario - $senha ";
         if((!empty($usuario)) AND (!empty($senha))){
             //Gerar senha e criptografar
-            echo password_hash($senha, PASSWORD_DEFAULT);
+            //echo password_hash($senha, PASSWORD_DEFAULT);
             //pesquisar cusuario no B.D
+            $result_usuario = "SELECT id, nome, email, senha, FROM usuarios WHERE usuario ='$usuario' LIMIT 1";
+            $resultado_usuario = mysqli_query($conn, $result_usuario);
+            if ($resultado_usuario){
+                $row_usuario = mysqli_fetch_assoc($resultado_usuario);
+                if(password_verify($senha,$row_usuario['senha'])){
+                    
+                } else {
+                    $_SESSION['msg'] = "LOGIN E SENHA : INCORRETO";
+                    header("Location: login.php");
+                }
+            }
         } else {
             $_SESSION['msg'] = "LOGIN E SENHA : INCORRETO";
             header("Location: login.php");
