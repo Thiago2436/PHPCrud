df6526f6a215ce18c444a401cb81301003373bc8
Thiago2436 <thiagomartins.florestal@gmail.com>
2023-01-26 19:54:49 -0300

    refatorando para projeto store de musica

--- index.php
+++ index.php
@@ -4,13 +4,14 @@
 <!DOCTYPE html>
 <html lang="pt-br">
 <head>
+    <link rel="stylesheet" href="Sstilo.css">
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Login</title>
 </head>
 <body>
-    <h2>Area Restrita</h2>
+    <h1>Area Restrita</h1>
     <?php
     
         if(isset($_SESSION['msg'])){
@@ -26,7 +27,17 @@
         <input type="text" name="senha" placeholder="Digite sua Senha"><br><br>
         
         <input type="submit" name="btnLogin" value="Acessar"><br><br>
-    </form>    
+    </form>  
+    <h1>
+    <?php
+
+        if(isset($_SESSION['loginErro'])){
+            echo $_SESSION['loginErro'];
+            unset($_SESSION['loginErro']);
+        }
+    
+    ?>
+    </h1>
     
 </body>
 </html>
--- valida.php
+++ valida.php
@@ -3,6 +3,11 @@ session_start();
 include_once("conex.php");
 if ((isset($_POST['usuario'])) && (isset($_POST['senha']))){
 
+    $usuario = mysqli_real_escape_string($conn, $_POST  ['usuario']);
+    $senha = mysqli_real_escape_string($conn, $_POST['senha']);
+    $senha = md5($senha);
+    
+
 }else{
     $_SESSION['loginerror'] = "Usuario ou senha invalida";
     header("Location: index.php");
