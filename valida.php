9cd1097fb73f5644bce4029f6cd1c66b36a68344
Thiago2436 <thiagomartins.florestal@gmail.com>
2023-01-31 21:31:05 -0300

    add busca no banco de dados php

--- valida.php
+++ valida.php
@@ -2,10 +2,14 @@
 session_start();
 include_once("conex.php");
 if ((isset($_POST['usuario'])) && (isset($_POST['senha']))){
-
+//sql injection
     $usuario = mysqli_real_escape_string($conn, $_POST  ['usuario']);
     $senha = mysqli_real_escape_string($conn, $_POST['senha']);
     $senha = md5($senha);
+//Buscar usuario no banco de dados 
+$sql ="SELECT * FROM Usuario WHERE usuario = '$usuario' && senha = '$senha' LIMIT 1";
+$result = mysqli_query($conn, $sql);
+$resultado = mysqli_fetch_assoc($result);    
     
 
 }else{
